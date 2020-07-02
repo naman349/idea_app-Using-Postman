699a47847b0a5dd76ad2f2644883e5adda553f2a
Naman Mittal <mnaman42@gmail.com>
2020-07-02 17:35:59 +0530

    first commit

---
+++ src/main/java/com/upgrad/idea_app/IdeaAppApplication.java
@@ -0,0 +1,14 @@
+package com.upgrad.idea_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Naman Mittal");
+		SpringApplication.run(IdeaAppApplication.class, args);
+	}
+
+}
